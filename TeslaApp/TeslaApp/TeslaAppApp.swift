516d78f0d7e3ebcba13d6832e68c0ca83706da6f
Natalia_Poftalnaya <poftnataly@gmail.com>
2024-05-13 23:02:50 +0300

    initial commit

---
+++ TeslaApp/TeslaApp/TeslaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaAppApp.swift
+//  TeslaApp
+//
+//  Created by Наталья Владимировна Пофтальная on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeslaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
